acda38f2e67298e78e54ffd2146264b8504aa5e4
jselezneva <juli.89@mail.ru>
2023-02-28 15:45:39 +0300

    Customize login background

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -60,7 +60,7 @@ export class Branding {
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
   static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static LoginTitle = 'Welcome to Esanum';
   static GetLoginSubTitle = (): null | string => {
     return null;
   };
